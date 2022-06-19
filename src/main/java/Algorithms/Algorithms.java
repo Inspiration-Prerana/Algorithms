944e531a9c89712803de0a8940ce17f9790b318e
Prerana <prebajracharya@gmail.com>
2022-06-19 21:38:34 +0545

    First

---
+++ src/main/java/Algorithms/Algorithms.java
@@ -0,0 +1,20 @@
+package Algorithms;
+
+public class Algorithms {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        ListNode l1 = new ListNode(2,new ListNode(3, new ListNode(4)));
+        ListNode l2 = new ListNode(5,new ListNode(3, new ListNode(2)));
+
+        int sum = 0;
+
+
+
+
+
+
+    }
+
+}
